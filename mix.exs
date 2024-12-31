751a9f51e163a9f5a97ef6464bdf79a4ae11bc29
Gizeta <i@gizeta.me>
2024-12-31 15:00:32 +0800

    chore: bump v0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SimpleKVaaS.MixProject do
   def project do
     [
       app: :simple_kvaas,
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
